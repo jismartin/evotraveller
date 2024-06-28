d8f3c27fcdd62d2d23c684461b5e7aa60f442569
jismartin <jismartin@gmail.com>
2024-06-28 17:52:38 +0200

    Correct a bug with the use of lists of lists

--- algorithm.py
+++ algorithm.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 import random
+import copy
+# Be carefull, in Python lists are passed by reference; a list of list is a list of references to lists
 
 # TSP hyperparameters
 grid_size = 10
@@ -10,19 +12,18 @@ def set_seed(seed):
     np.random.seed(seed)
     random.seed(seed)
 
-# Places to visit
+# Places to visit (return a list of locations of the TSP)
 def places(grid_size,n_places):
-    #np.random.seed(seed)
     points = list(zip(np.random.randint(0,grid_size,n_places),np.random.randint(0,grid_size,n_places)))
     while (0,0) in points:
         points = list(zip(np.random.randint(0,grid_size,n_places),np.random.randint(0,grid_size,n_places)))
     return points
 
-# Add the start and end points to a sequence
+# Add the origin and destination to the sequence-list
 def add_extremes(sequence):
     return [(0,0)] + sequence + [(0,0)]
 
-# Fitness function
+# Fitness function (works for one sequence-list)
 def distance(sequence):
     path=add_extremes(sequence)
     return sum(np.sqrt((path[i][0] - path[i+1][0])**2 + (path[i][1] - path[i+1][1])**2) 
@@ -30,7 +31,7 @@ def distance(sequence):
 def fitness(solution):
     return 1 / distance(solution) # shorter distence higher fitness
 
-# Intial Population
+# Intial Population (return a list of lists!)
 def initial_population(places,population_size):
     return [random.sample(places, len(places)) for _ in range(population_size)]
 
@@ -40,10 +41,10 @@ def random_tournament(population, tournament_size):
     for _ in range(len(population)):
         tournament = random.sample(population, tournament_size)
         winner = max(tournament, key=fitness)
-        selected_for_next_round.append(winner)
+        selected_for_next_round.append(winner.copy()) # Be careful, a copy of a list to get a new solution
     return selected_for_next_round
 
-# Crossover (ox crossover)
+# Crossover (ox crossover) returns two offspring solutions from two parents
 # Ox ccopy
 def ox_copy(parent1,parent2,start,end):
     size = min(len(parent1), len(parent2))
@@ -79,32 +80,36 @@ def mutate(solution, mutation_rate):
 
 # Genetic algortihm
 def genetic_algorithm(targets, population_size, generations, tournament_size, mutation_rate, elitism,crossover):
+    
+    # Initialize the population
     population = initial_population(targets,population_size)
     best_solution=max(population, key=fitness)
-    fitness_evolution=[]
-    fitness_evolution.append(fitness(max(population, key=fitness)))
+    fitness_evolution=[fitness(best_solution)]
     
-    # Keep simulating generations
+    # Track simulating generations
     df_simulation = pd.DataFrame({'c%i'%i:[add_extremes(population[i])] for i in range(population_size)})
 
-    e = int(elitism*population_size)
-    e = e if (e % 2 == 0) else e+1 # size of elitism: even number
+    e = max(1, int(elitism*population_size))
+    e = e if (e % 2 == 0) else e + 1 # size of elitism: even number
     nc = int(crossover*population_size) 
-    nc = nc if (nc % 2 == 0) else nc+1 # size of crossover: even number
+    nc = nc if (nc % 2 == 0) else nc + 1 # size of crossover: even number
 
     for t in range(generations):
-        population = sorted(population,key=fitness)
-        population.reverse()
+        
+        # Sort population by fitness in descending order
+        population = sorted(population, key=fitness, reverse=True)
 
+        # Create next generation list
+        
         # (1) elite (no mutation)
-        next_generation = population[:e]
+        next_generation = copy.deepcopy(population[:e]) # Keep elite individuals (best solutions)
 
         # Selection
         selection = random_tournament(population, tournament_size)
-        parents=random.sample(selection, len(selection))
+        parents=random.sample(selection, len(selection)) # shuffle the selected parents
         
         # (2) direct descendants with mutation
-        for i in range(population_size-nc-e):
+        for i in range(population_size - nc - e):
             parent = parents.pop()
             next_generation.append(mutate(parent,mutation_rate))
 
@@ -116,18 +121,14 @@ def genetic_algorithm(targets, population_size, generations, tournament_size, mu
             next_generation.append(mutate(child1,mutation_rate))
             next_generation.append(mutate(child2,mutation_rate))
  
-        population = next_generation
-
-        if best_solution not in population:
-            print('Best solution lost!')
-
+        # Update population
+        population = copy.deepcopy(sorted(next_generation, key=fitness, reverse=True))
         fitness_evolution.append(fitness(max(population, key=fitness)))
-        if fitness(max(population, key=fitness)) > fitness(best_solution):
-            best_solution=max(population, key=fitness) 
 
-        population = sorted(population,key=fitness)
-        population.reverse()
+        # Track simulation data        
         df_simulation=pd.concat([df_simulation,
                                  pd.DataFrame({'c%i'%i:[add_extremes(population[i])] for i in range(population_size)})],ignore_index=True)
+
+    best_solution=max(population, key=fitness)
 
     return add_extremes(best_solution), fitness_evolution, df_simulation
